7be3c4782cb9582c3c5ee823edf8c3c84a07e64e
SteffXD <steffen123.l.a@gmail.com>
2021-10-24 17:25:45 +0200

    Session timeout 30min

--- app/routes.py
+++ app/routes.py
@@ -158,4 +158,4 @@ def post(post_id=None):
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=5)
+    app.permanent_session_lifetime = timedelta(minutes=30)
