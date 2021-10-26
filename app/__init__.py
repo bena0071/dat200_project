b13e25ae817926b471b2fb61412e213f9114c2a9
bena0071 <yodasanei@gmail.com>
2021-10-26 18:59:37 +0200

    lol

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,7 @@ from config import Config
 import os
 
 app = Flask(__name__)
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
-    uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
