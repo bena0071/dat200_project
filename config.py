cca0fef57ab805865d849051f336c58c0602c924
bena0071 <yodasanei@gmail.com>
2021-10-26 17:38:43 +0200

    lol

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('asdaldfi2ifjadfi9u8') or 'a1d2f3g4'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-            'postgres://', 'postgresql://')
+                    'postgres://', 'postgresql://') or \
+                    'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RECAPTCHA3_PUBLIC_KEY = "6LeuMsMcAAAAAJ4BIp_YEr254_bljnCTH9SWrOY4"
     RECAPTCHA3_PRIVATE_KEY = "6LeuMsMcAAAAAHLg4ZHovJMJBidpo_LwcCZGSq6K"
